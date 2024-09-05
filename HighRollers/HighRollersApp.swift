271e5ac98d42db974a83930a5354899354e1ebce
Adam Sayer <asayer82@icloud.com>
2024-09-05 12:45:19 +1000

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Adam on 5/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
